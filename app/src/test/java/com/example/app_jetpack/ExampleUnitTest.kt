512dc33b1fe38d2e050e820753edfea74a685878
Roanderson Pinheiro <roandersonfelipe@gmail.com>
2020-11-18 09:26:12 -0300

    change file

--- app/src/test/java/com/example/app_jetpack/ExampleUnitTest.kt
+++ app/src/test/java/com/example/app_jetpack/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
 package com.example.app_jetpack
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.Assert.assertEquals
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
