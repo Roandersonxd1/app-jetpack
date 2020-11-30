ee0b04a70a71820deada34cdfab389d53b93a21e
Roanderson Pinheiro <roandersonfelipe@gmail.com>
2020-11-30 14:43:35 -0300

    Added animations

--- app/src/main/java/com/example/app_jetpack/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/app_jetpack/ui/start/StartFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.app_jetpack.R
+import com.example.app_jetpack.extensions.navigateWithAnimations
 import kotlinx.android.synthetic.main.fragment_start.*
 
 class StartFragment : Fragment() {
@@ -24,7 +25,7 @@ class StartFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         buttonNext.setOnClickListener {
-            findNavController().navigate(R.id.action_startFragment_to_profileFragment)
+            findNavController().navigateWithAnimations(R.id.action_startFragment_to_profileFragment)
         }
     }
 }
